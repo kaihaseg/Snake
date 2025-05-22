49281bf753444f8cd57f9935daf77c17e3682797
Kai <142910428+kaihaseg@users.noreply.github.com>
2025-05-23 02:37:19 +0300

    Update snake.h

--- include/snake.h
+++ include/snake.h
@@ -1,1 +1,20 @@
+#include <vector>
+#include "dir.h"
+
+struct SnakeSegment
+{
+    int x, y;
+};
+
+class Snake
+{
+public:
+    std::vector <SnakeSegment> segments;
+    Dir dir;
+
+    Snake();
+    void move();
+    void grow();
+    bool checkCollision() const;
+};
 
